7adb3cfa2cf72124dc393f0795ff1cfb0296c227
peerawattae <peerawat.thee@ku.th>
2026-04-13 10:55:02 +0700

    Create the strategy layer as a new module

---
+++ core/generation/__init__.py
@@ -0,0 +1,9 @@
+from .base import SongGeneratorStrategy, GenerationRequest, GenerationResult
+from .mock import MockSongGeneratorStrategy
+
+__all__ = [
+    "SongGeneratorStrategy",
+    "GenerationRequest",
+    "GenerationResult",
+    "MockSongGeneratorStrategy",
+]
