c916707880eb81d905932890a7a553c80d7a58e3
peerawattae <peerawat.thee@ku.th>
2026-04-27 10:38:50 +0700

    add feature creat song page

--- core/views/pages.py
+++ core/views/pages.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 
 def library_page(request):
     return render(request, "core/library.html")
+
+def create_page(request):
+    return render(request, "core/create.html")
--- music_ai/urls.py
+++ music_ai/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
-from core.views import library_page
+from core.views.pages import library_page, create_page
 
 urlpatterns = [
     path("admin/",   admin.site.urls),
     path("api/",     include("core.urls")),
     # Frontend pages
     path("library/",                      library_page,     name="library"),
+    path("create/",                       create_page,      name="create"),
 ]
